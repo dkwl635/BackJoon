f219e50dadf48db60caafb4e8991500d79d8aae9
김재완 <82872150+dkwl635@users.noreply.github.com>
2025-01-07 09:35:51 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N = 10;
+    int Max = 0;
+    int People = 0;
+    for(int i =0; i < N; i++)
+    {
+        int Add , Minus;
+        cin >> Minus >> Add;
+        People -= Minus;
+        People += Add;
+        
+        if(Max < People)
+        {
+            Max = People;
+        }
+        
+    }
+    cout << Max;
+    return 0;
+}
