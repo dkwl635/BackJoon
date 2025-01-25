c92a955675456ead50c2a3cd6362bb84eb52dd18
김재완 <82872150+dkwl635@users.noreply.github.com>
2025-01-25 20:57:23 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.cpp
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.cpp
@@ -7,11 +7,14 @@ using namespace std;
 int solution(string binomial) {
     int answer = 0;
     
+    //문자열 분리를 위한 
     istringstream iss(binomial);    
     int a ,b ;
     string op;
-    
+    //분리된 데이터 
     iss >> a>>op>>b;
+    
+    //계산
     if(op == "+")
     {
         answer = a + b;
