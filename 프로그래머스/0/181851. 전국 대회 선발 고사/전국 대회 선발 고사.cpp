4498a633b191eef6e3ce590bd7a1ca18945bae97
김재완 <82872150+dkwl635@users.noreply.github.com>
2025-02-01 12:37:45 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cpp
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cpp
@@ -6,10 +6,12 @@
 using namespace std;
 
 int solution(vector<int> rank, vector<bool> attendance) {
-    int answer = 0;
-    
+
+    //출전 가능한 학생 저장
     vector<int> student;
+    //학생의 순서 기억을 위한
     map<int , int> student_Num;
+    //학생과 순서 기억
     for(int i ; i < rank.size(); i++)
     {
         if(attendance[i]) { 
@@ -17,8 +19,10 @@ int solution(vector<int> rank, vector<bool> attendance) {
             student_Num[rank[i]] = i;
         }
     }
+    //정렬
     sort(student.begin() , student.end());
     
+    //abc 지정
     int a , b,c;
     a = student_Num[student[0]];
     b = student_Num[student[1]];
