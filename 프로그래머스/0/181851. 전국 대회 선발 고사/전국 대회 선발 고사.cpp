9a3831e6990e126a488750b145485780c6dc23ff
김재완 <82872150+dkwl635@users.noreply.github.com>
2025-02-01 12:35:42 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> rank, vector<bool> attendance) {
+    int answer = 0;
+    
+    vector<int> student;
+    map<int , int> student_Num;
+    for(int i ; i < rank.size(); i++)
+    {
+        if(attendance[i]) { 
+            student.push_back(rank[i]);
+            student_Num[rank[i]] = i;
+        }
+    }
+    sort(student.begin() , student.end());
+    
+    int a , b,c;
+    a = student_Num[student[0]];
+    b = student_Num[student[1]];
+    c = student_Num[student[2]];    
+    
+    return 10000 * a + 100 * b + c;
+}
