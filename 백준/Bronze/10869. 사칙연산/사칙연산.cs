bb72a964f4f11be633e273301f4f4dc6154b6bfb
김재완 <82872150+dkwl635@users.noreply.github.com>
2025-03-08 10:24:47 +0900

    [Bronze V] Title: 사칙연산, Time: 64 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace 백준
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
+            int a = int.Parse(ss[0]);
+            int b = int.Parse(ss[1]);
+
+            Console.WriteLine(a + b );
+            Console.WriteLine(a - b );
+            Console.WriteLine(a * b );
+            Console.WriteLine(a / b );
+            Console.WriteLine(a % b );
+        }
+    }
+}
