073ca81216d334f53d76c08cce3b9640a8115bbe
김재완 <82872150+dkwl635@users.noreply.github.com>
2025-04-10 17:54:56 +0900

    [Gold II] Title: 문제집, Time: 28 ms, Memory: 4216 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+
+using namespace std;
+
+int n, m, u, v;
+
+vector<vector<int>> graph;
+vector<int> InDegree;
+vector<int> Order;
+
+void toplogySort()
+{
+	//queue<int> q;
+	priority_queue<int ,vector<int> ,greater<>> q;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		if (InDegree[i] == 0)
+		{
+			q.push(i);
+		}
+	}
+
+	while (!q.empty())
+	{
+		int node = q.top();
+		q.pop();
+		Order.emplace_back(node);
+
+		for (int& neighborNode : graph[node])
+		{
+			InDegree[neighborNode]--;
+			if (InDegree[neighborNode] == 0)
+			{
+				q.push(neighborNode);		
+				//Order.emplace_back(neighborNode);
+
+			}
+		}
+	}
+
+	if (Order.size() != n) { return; }
+
+
+	for (int i = 0; i < Order.size(); ++i)
+	{
+		cout << Order[i] << " ";
+	}
+
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n >> m;
+
+	graph = vector<vector<int>>(n + 1);
+	InDegree = vector<int>(n + 1 , 0);
+
+
+	for (int i = 0; i < m; ++i)
+	{
+		cin >> u >> v;
+		graph[u].emplace_back(v);
+
+		InDegree[v]++;
+
+	}
+
+	toplogySort();
+
+	return 0;
+}
+
