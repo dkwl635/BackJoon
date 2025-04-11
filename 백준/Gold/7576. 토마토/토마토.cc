0f3dc2f18201c21101058d2cb0ee3645de4b85e2
김재완 <82872150+dkwl635@users.noreply.github.com>
2025-04-11 10:38:44 +0900

    [Gold V] Title: 토마토, Time: 292 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -14,45 +14,13 @@ vector<vector<bool>> visited;
 int dirX[4] = { 0,0,1,-1 };
 int dirY[4] = { 1,-1,0,0 };
 
-
-
-void CheckTomato(int x ,int y , bool& find)
-{
-	if (x < 0 || x >= n || y < 0 || y >= m)
-		return;
-
-	if (visited[x][y] == true) return;
-	if (map[x][y] == -1)
-	{
-		return;
-	}
-
-	visited[x][y] = true;
-
-	if (map[x][y] == 1)
-	{
-		find = true;
-		q.push({ x,y });
-	}
-	else if (map[x][y] == 0)
-	{
-		tomato++;
-	}
-
-
-	for (int i = 0; i < 4; ++i)
-	{
-		int newX = x + dirX[i];
-		int newY = y + dirY[i];
-		CheckTomato(newX, newY, find);
-	}
-}
 void NextDay()
-{
-	while (tomato > 0)
+{	
+	result = -1;
+	do
 	{
-		result++;
 		queue<pair<int, int>> Newq;
+		++result;
 		while (!q.empty())
 		{
 			int x = q.front().first;
@@ -69,46 +37,48 @@ void NextDay()
 
 				if (map[newX][newY] == 1 || map[newX][newY] == -1)
 					continue;
-				tomato--;
+				--tomato;
 				map[newX][newY] = 1;
 				Newq.push({ newX , newY });
 			}
 		}
 
 		q = Newq;
-	}
+	
+
+	} while (!q.empty());
+
 }
 
 int main()
 {
-
 	tomato = 0;
-	result = 0;
-
+	
 	cin >> m >> n;
 	map = vector<vector<int>>(n, vector<int>(m, 0));
 	visited = vector<vector<bool>>(n, vector<bool>(m, false));
 	for (int i = 0; i < n; ++i)
 		for (int j = 0; j < m; ++j)
-			cin >> map[i][j];
-
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < m; ++j)
 		{
-			if (visited[i][j] == true) continue;
-			if (map[i][j] == -1) continue;
-
-			bool t = false;
-			CheckTomato(i, j , t);
-
-			if (t == false)
+			int v;
+			cin >> v;
+			if (v == 0)
 			{
-				cout << -1;
-				return 0;
+				++tomato;
 			}
+			else if (v == 1)
+			{
+				q.push({ i,j });
+			}
+
+			map[i][j] = v;
 		}
 
+	
 	NextDay();
+	if (tomato > 0)
+		cout << -1;
+	else
 	cout << result;
 	return 0;
 }
