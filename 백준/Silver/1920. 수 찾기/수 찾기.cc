629397cd25096dc5798c20b9e44918937f9b996e
김재완 <82872150+dkwl635@users.noreply.github.com>
2025-04-17 17:32:46 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,38 +1,63 @@
 #include <iostream>
-#include <unordered_map>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
+
+int  n, m;
+vector<int> nums;
+
+bool BinarySerch(int x)
+{
+    int start = 0;
+    int end = n - 1;
+    while (start <= end)
+    {
+        int mid = start + ((end - start) / 2);
+
+        if (nums[mid] == x)
+        {
+            return true;
+        }
+
+        if (x > nums[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+
+    return false;
+}
 
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
- 
-    int n ,x;
-    unordered_map<int , int> hash_map;
-    
+    cin.tie(0);
+    cout.tie(0);
+
     cin >> n;
-    for(int i = 0 ; i < n; i++)
+
+    nums = vector<int>(n);
+    for (int i = 0; i < n; ++i)
     {
-        cin >> x;
-        hash_map[x] = 1;
+        cin >> nums[i];
     }
-    
-    cin >> n;
-    for(int i = 0 ; i < n; i++)
+
+    sort(nums.begin(), nums.end());
+
+
+    cin >> m;
+    int x;
+    for (int i = 0; i < m; ++i)
     {
         cin >> x;
-        if( hash_map[x] == 1)
-        {
-            cout << 1 ;
-        }
-        else
-        {
-            cout << 0 ;
-        }
         
-        cout << "\n";
+        cout << (BinarySerch(x) ? 1 : 0) << "\n";
     }
-    
-    return 0;
+
+
 }
